cf41884ec6415e38741ae81650393ab1f8bc37e4
ModernTinTin1 <92940727+Wespiseb@users.noreply.github.com>
2024-02-03 14:31:07 -0600

    all arm constants excluding pitch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,15 +10,15 @@ public final class Constants {
       public static final int kPitchMotorId = 0101010;
     }
     public static class ShooterMotors{
-      public static final int kTopShooterMotorId = 00001;
-      public static final int kBottomShooterMotorId = 00002;
+      public static final int kTopShooterMotorId = 62;
+      public static final int kBottomShooterMotorId = 44;
     }
     public static class PushMotor {
-      public static final int kPushMotorId = 0000302;
+      public static final int kPushMotorId = 52;
     }
     public static class IntakeMotors {
-      public static final int kTopIntakeMotorId = 44;
-      public static final int kBottomIntakeMotorId = 58;
+      public static final int kTopIntakeMotorId = 41;
+      public static final int kBottomIntakeMotorId = 60;
     }
 
     
