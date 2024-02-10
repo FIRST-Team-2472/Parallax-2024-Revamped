19b03514a3c5268ab7da6966c6f6a69a21da3792
Charlie <charlie.stein2027@gmail.com>
2024-02-10 10:42:14 -0600

    reset heading added

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj.Joystick;
@@ -53,6 +55,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    new JoystickButton(rightJoystick, 4).onTrue(new InstantCommand(swerveSubsystem :: zeroHeading));
   }
 
   public Command getAutonomousCommand() {
