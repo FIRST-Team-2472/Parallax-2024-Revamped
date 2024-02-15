8c5a466f5401c405bd3224a241eec96f1f5bdf95
Jacob Black <bikerduck7@gmail.com>
2024-02-15 17:09:41 -0600

    Equation to stop the arm falling added
    2/15/24

--- src/main/java/frc/robot/subsystems/ArmMotorsSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmMotorsSubsystem.java
@@ -19,6 +19,8 @@ public class ArmMotorsSubsystem extends SubsystemBase {
     private CANSparkMax intakeBottomMotor = new CANSparkMax(IntakeMotors.kBottomIntakeMotorId, MotorType.kBrushless);
     private PIDController pitchPIDController = new PIDController(PitchMotor.kPitchMotorKP, 0, 0);
     private AnalogEncoder pitchMotorEncoder;
+    public double baseIdleForce;
+
     public ArmMotorsSubsystem(AnalogEncoder pitchMotorEncoder) {
         this.pitchMotorEncoder = pitchMotorEncoder;
         shooterTopMotor.restoreFactoryDefaults();
@@ -30,11 +32,37 @@ public class ArmMotorsSubsystem extends SubsystemBase {
         pitchMotor.setIdleMode(com.revrobotics.CANSparkBase.IdleMode.kBrake);
         pitchMotor.setSoftLimit(SoftLimitDirection.kReverse, (float) PitchMotor.kPitchEncoderReverseLimit);
         pitchMotor.setSoftLimit(SoftLimitDirection.kForward, (float) PitchMotor.kPitchEncoderForwardLimit);
-        
+
+    }
+
+    @Override
+    public void periodic() {
+
+        // To prevent the arm from falling while idling, we add a base force that
+        // prevents the arm from falling, this should always be added to any movement
+        // and be clamped to prevent values that are too high. This basically negates
+        // gravity.
+        baseIdleForce = PitchMotor.kPitchBaseIdleForce
+                * Math.sin((pitchMotorEncoder.getDistance() / 360) * (2 * Math.PI));
+
+        // If there is any sort of jittering, shaking, or no movement, try running the
+        // runPitchMotor() inside here. This will prevent setting the base idle force to
+        // the point of not moving, unless it is not moving. periodic() does run before
+        // execute, but just in case.
+        if (pitchMotor.get() == 0) {
+            // runPitchMotor(0);
+        }
+
+        // Pass in 0, as runPitchMotor() already adds the baseIdleForce
+        runPitchMotor(0);
+    }
+
+    double addBaseIdleForce(double motorSpeed) {
+        return clamp(motorSpeed + baseIdleForce, -1.0, 1.0);
     }
 
     public void runPitchMotor(double motorSpeed) {
-        pitchMotor.set(motorSpeed);
+        pitchMotor.set(addBaseIdleForce(motorSpeed));
     }
 
     public void runShooterMotors(double motorSpeed) {
@@ -54,7 +82,11 @@ public class ArmMotorsSubsystem extends SubsystemBase {
     public void runPitchMotorWithKP(double angleDeg) {
 
         double speed = pitchPIDController.calculate(pitchMotorEncoder.getDistance(), angleDeg);
-        
+
         runPitchMotor(speed);
+    }
+
+    double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
     }
 }
