a4e2ae2c138903b861aa9ea3b71f5a4a5aaf5641
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2024-02-24 08:15:56 -0600

    Trying things for running the arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -68,6 +68,8 @@ public class RobotContainer {
   private void configureBindings() {
     new JoystickButton(rightJoystick, 4).onTrue(new InstantCommand(swerveSubsystem :: zeroHeading));
     new CommandXboxController(OperatorConstants.kXboxControllerPort).axisGreaterThan(1, .5).whileTrue(new runShooter(armSubsystem));
+    new CommandXboxController(OperatorConstants.kXboxControllerPort).leftTrigger(0.5).whileTrue(new runShooter(armSubsystem));
+    new CommandXboxController(OperatorConstants.kXboxControllerPort).leftBumper().onTrue(new runShooter(armSubsystem));
 
     new CommandXboxController(OperatorConstants.kXboxControllerPort).a().onTrue(new SetArmPitchCmd(armSubsystem, ArmMotorsConstants.PitchMotor.kPitchMotorIntakePresetAngle));
     new CommandXboxController(OperatorConstants.kXboxControllerPort).b().onTrue(new SetArmPitchCmd(armSubsystem, ArmMotorsConstants.PitchMotor.kPitchMotorSpeakerPresetAngle));
