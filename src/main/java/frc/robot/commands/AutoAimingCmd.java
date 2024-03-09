1c84d2b7cd1850bb28c1d616a79260110d7d5cc2
Aedyn <aedyn@hylandernet.net>
2024-03-09 11:17:39 -0600

    pitch aiming

--- src/main/java/frc/robot/commands/AutoAimingCmd.java
+++ src/main/java/frc/robot/commands/AutoAimingCmd.java
@@ -5,26 +5,36 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.LimelightHelpers;
 import frc.robot.Constants.AutoAimingConstants;
-import frc.robot.subsystems.ArmSubsystems.PitchMotorSubsystem;
+import frc.robot.LimelightHelpers.LimelightResults;
+import frc.robot.commands.*;
+import frc.robot.commands.DefaultCommands.*;
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.ArmSubsystems.*;
 import frc.robot.subsystems.swerveExtras.PosPose2d;
 
 public class AutoAimingCmd extends Command {
-    //private double x, y, z;
-    private double z = 10;
-    private double y = 7;
-    private double x = 18;
+    private double x, y, z;
+    // private double z = 10;
+    // private double y = 7;
+    // private double x = 18;
     private double pitchAngle, yawAngle;
     private Pose3d targetPoseCameraSpace;
     private PitchMotorSubsystem pitchSubsystem;
+    private ShootingMotorSubsystem shooterSubsystem;
+    private IntakeMotorSubsystem intakeSubsystem;
     private SwerveModule swerveModule;
     private SwerveDriveToPointCmd swerveDriveToPointCmd;
     private SwerveRotateToAngle swerveRotateToAngle;
     private PosPose2d posPose2d;
+    private boolean isDone = false;
 
-    public AutoAimingCmd() {
-
+    public AutoAimingCmd(PitchMotorSubsystem pitchSubsystem, ShootingMotorSubsystem shooterSubsystem, IntakeMotorSubsystem intakeSubsystem) {
+        this.pitchSubsystem = pitchSubsystem;
+        this.intakeSubsystem = intakeSubsystem;
+        this.shooterSubsystem = shooterSubsystem;
     }
 
     @Override
@@ -34,18 +44,29 @@ public class AutoAimingCmd extends Command {
 
     @Override
     public void execute() {
-    targetPoseCameraSpace = LimelightHelpers.getTargetPose3d_CameraSpace("limelight-shooter");
-    // TODO Filter out all targets exept the one we want
-    
-        //x = targetPoseCameraSpace.getX();
-        //y = targetPoseCameraSpace.getY();
-        //z = targetPoseCameraSpace.getZ();
+        targetPoseCameraSpace = LimelightHelpers.getTargetPose3d_CameraSpace("limelight-shooter");
+        LimelightResults llr = LimelightHelpers.getLatestResults("limelight-shooter");
+        // TODO Filter out all targets exept the one we want
 
-        pitchAngle = Units.radiansToDegrees(Math.atan(y / z));
-        yawAngle = Units.radiansToDegrees(Math.atan(x / z));
+        if (llr.targetingResults.targets_Fiducials.length > 0) {
+            x = targetPoseCameraSpace.getX();
+            y = targetPoseCameraSpace.getY();
+            z = targetPoseCameraSpace.getZ();
 
-        System.out.println(armAimingEquation(pitchAngle));
-        
+            pitchAngle = Units.radiansToDegrees(Math.atan2(z, y));
+            yawAngle = Units.radiansToDegrees(Math.atan2(z, x));
+
+
+
+            System.out.println(armAimingEquation(pitchAngle));
+
+            new SequentialCommandGroup(
+                    new SetArmPitchCmd(pitchSubsystem, armAimingEquation(pitchAngle)),
+                    new runShooter(shooterSubsystem, intakeSubsystem, .5) // FIXME Get correct speed
+            );
+        }
+
+        isDone = true;
     }
 
     @Override
@@ -55,7 +76,7 @@ public class AutoAimingCmd extends Command {
 
     @Override
     public boolean isFinished() {
-        return true;
+        return isDone;
     }
 
     public double armAimingEquation(double angle) {
