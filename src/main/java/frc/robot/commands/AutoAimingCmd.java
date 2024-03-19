2cabab0f69a3a186d88dba43b84e012031bfb479
Jacob Black <bikerduck7@gmail.com>
2024-03-19 15:21:41 -0500

    Clean Up Imports

--- src/main/java/frc/robot/commands/AutoAimingCmd.java
+++ src/main/java/frc/robot/commands/AutoAimingCmd.java
@@ -1,33 +1,15 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
-
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.CommandSequences;
-import frc.robot.LimelightHelpers;
 import frc.robot.Constants.AutoAimingConstants;
-import frc.robot.LimelightHelpers.LimelightResults;
-import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
-import frc.robot.commands.*;
-import frc.robot.commands.DefaultCommands.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.ArmSubsystems.*;
-import frc.robot.subsystems.swerveExtras.PosPose2d;
-import frc.robot.Constants.AutoAimingConstants;
+
 
 public class AutoAimingCmd extends Command {
-    private double x, y, z, tx, ty;
-    // private double z = 10;
-    // private double y = 7;
-    // private double x = 18;
     private double pitchAngle, yawAngle;
-    // private Pose3d targetPoseCameraSpace;
     private PitchMotorSubsystem pitchSubsystem;
     private ShootingMotorSubsystem shooterSubsystem;
     private IntakeMotorSubsystem intakeSubsystem;
@@ -35,12 +17,8 @@ public class AutoAimingCmd extends Command {
 
     private CommandSequences commandSequences;
     private Command rotateNShoot;
-    // private SwerveDriveToPointCmd swerveDriveToPointCmd;
-    // private SwerveRotateToAngle swerveRotateToAngle;
     private Pose2d robotPos;
     private double distanceFromSpeaker;
-
-    private boolean isDone = false;
 
     public AutoAimingCmd(SwerveSubsystem swerveSubsystem, PitchMotorSubsystem pitchSubsystem,
             ShootingMotorSubsystem shooterSubsystem,
@@ -70,13 +48,13 @@ public class AutoAimingCmd extends Command {
             distanceFromSpeaker = getDistance(robotPos.getX(), robotPos.getY(),
                     AutoAimingConstants.blueSpeakerPos.getX(),
                     AutoAimingConstants.blueSpeakerPos.getY());
-            yawAngle = ngrdjfejsjflues(AutoAimingConstants.blueSpeakerPos.getX(),
+            yawAngle = flues(AutoAimingConstants.blueSpeakerPos.getX(),
                     AutoAimingConstants.blueSpeakerPos.getY(), robotPos.getX(), robotPos.getY());
         } else {
             distanceFromSpeaker = getDistance(robotPos.getX(), robotPos.getY(),
                     AutoAimingConstants.redSpeakerPos.getX(),
                     AutoAimingConstants.redSpeakerPos.getY());
-            yawAngle = ngrdjfejsjflues(AutoAimingConstants.redSpeakerPos.getX(),
+            yawAngle = flues(AutoAimingConstants.redSpeakerPos.getX(),
                     AutoAimingConstants.redSpeakerPos.getY(), robotPos.getX(), robotPos.getY());
         }
 
@@ -90,23 +68,13 @@ public class AutoAimingCmd extends Command {
     }
 
     @Override
-    public void execute() {
-
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-
-    }
-
-    @Override
     public boolean isFinished() {
         return true;
     }
 
     public double distanceToAngle(double Distance) {
         return 94.9 * Math.exp(-0.142 * Distance);// google equation
-        // return 0.26*Distance*Distance - 11.19*Distance + 93.06; calculator eqation
+        // return 0.26*Distance*Distance - 11.19*Distance + 93.06; calculator equation
     }
 
     double getDistance(double x1, double y1, double x2, double y2) {
@@ -117,7 +85,7 @@ public class AutoAimingCmd extends Command {
         return xPos < 8.25;
     }
 
-    double ngrdjfejsjflues(double x1, double y1, double x2, double y2) {
+    double flues(double x1, double y1, double x2, double y2) {
         return -(Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI));
     }
 
