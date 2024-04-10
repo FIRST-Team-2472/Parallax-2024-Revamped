4619df4a7d4f17b5c5e86b777eb55cc4b1c147b5
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2024-04-09 19:02:47 -0500

    Naming of the 3 note auto in pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,7 +44,8 @@ public class RobotContainer {
   shootFromFurtherAway = "Shoot from note position", justMovePosition2tonote1 = "Move to note 1 from the front of subwoofer",
   justMovePosition2tonote3 = "Move to note 3 from front of subwoofer", justMovePosition1tonote1 = "Move to note 1 from position 1",
   justMovePosition1tonote2 = "Move to note 2 from position 1", justMovePosition1tonote3 = "Move to note 3 from position 1",  
-  fiveNoteFromPosition2 = "Five note auto collecting four notes closest to amp", threeNoteMiddle = "Three notes starting center", AutoOne = "AutoOne";
+  fiveNoteFromPosition2 = "Five note auto collecting four notes closest to amp", threeNoteMiddle = "Three notes starting center", 
+  StwoNtwoNone = "Three Note Auto from position 2 to note 2 to note 1";
   
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
@@ -99,7 +100,7 @@ public class RobotContainer {
     m_chooser.addOption(justMovePosition1tonote2, justMovePosition1tonote2);
     m_chooser.addOption(justMovePosition1tonote3, justMovePosition1tonote3);
     m_chooser.addOption(fiveNoteFromPosition2, fiveNoteFromPosition2);
-    m_chooser.addOption(AutoOne, AutoOne);
+    m_chooser.addOption(StwoNtwoNone, StwoNtwoNone);
 
     ShuffleboardTab driverBoard = Shuffleboard.getTab("Driver Board");
     driverBoard.add("Auto choices", m_chooser).withWidget(BuiltInWidgets.kComboBoxChooser);
@@ -151,8 +152,8 @@ public class RobotContainer {
        
       m_autoSelected = m_chooser.getSelected();
 
-      if(m_autoSelected == AutoOne)
-        return AutoBuilder.buildAuto(AutoOne);
+      if(m_autoSelected == StwoNtwoNone)
+        return AutoBuilder.buildAuto(StwoNtwoNone);
 
       if (m_autoSelected == testingPath)
         return new ParallelCommandGroup(commandSequences.shootWaitNCrossTheLineAmpSide(swerveSubsystem, pitchMotorSubsystem, shootingMotorSubsystem, intakeMotorSubsystem));
